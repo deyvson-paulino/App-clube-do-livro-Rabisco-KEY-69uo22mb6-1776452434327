8e02c5c389135ff703c681dc9e45537e1e5bf68f
Deyvson Paulino <deyvson.uni@gmail.com>
2026-04-17 20:16:32 -0300

    Add files via upload

---
+++ js/array.js
@@ -0,0 +1,23 @@
+// Array de usuários
+const usuarios = [
+  { usuario: "admin", senha: "admin123", tipo: "admin" },
+  { usuario: "deyvson", senha: "user123", tipo: "pai_rico_pai_pobre" },
+  { usuario: "malu", senha: "123", tipo: "club_do_livro" },
+  { usuario: "usuario", senha: "teste", tipo: "pai_rico_pai_pobre" }
+];
+
+// Função para retornar os usuários com length
+/*function getUsuarios() {
+  console.log("Total de usuários:", usuarios.length);
+  return usuarios;
+}*/
+
+// Exporta para outros scripts
+//export { getUsuarios };
+
+/*
+admin
+cursod
+moderador
+premium
+normal*/
